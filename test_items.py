1514d048cf0435a2d938074051779ca47a222b41
Nikita Yuzva <yuzva71@mail.ru>
2023-03-01 01:41:01 +0200

    Find 'Add to cart' button

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_button_add_to_cart(browser):
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     add_to_cart_button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
     assert add_to_cart_button is not None, "'Add to cart button' not found!"
